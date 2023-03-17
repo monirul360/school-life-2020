c2226a6e8df4239d59af57e3d56cd6bf89502cc1
Monirul360e <monirulislam65361@gmail.comm>
2023-03-18 00:11:40 +0600

    gallary.json file photo url add

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -7,8 +7,9 @@ import Photo from "./Photo";
 const Home = () => {
   const [photo, setPhoto] = useState([]);
   const [monirul, setmonirul] = useState(null);
+  //photo api https://school-life.onrender.com/photo
   useEffect(() => {
-    fetch("https://school-life.onrender.com/photo")
+    fetch("gallary.json")
       .then((res) => res.json())
       .then((data) => setPhoto(data));
   }, []);
