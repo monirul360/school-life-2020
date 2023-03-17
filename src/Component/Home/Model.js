251eca3defdd2f8076cbc2a87b35f41f02641fed
Monirul360e <monirulislam65361@gmail.comm>
2023-03-18 02:20:18 +0600

    photoviewer setup

--- src/Component/Home/Model.js
+++ src/Component/Home/Model.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Model = ({ monirul }) => {
+  const [zoom, setZoom] = useState(1);
+
+  const handleZoomIn = () => {
+    setZoom(prevZoom => prevZoom + 0.1);
+  };
+
+  const handleZoomOut = () => {
+    setZoom(prevZoom => prevZoom - 0.1);
+  };
   return (
     <div
       class="modal fade"
@@ -11,16 +20,20 @@ const Model = ({ monirul }) => {
     >
       <div class="modal-dialog">
         <div class="modal-content">
-          <div class="modal-header">
+          <div style={{ border: "none" }} class="modal-header">
             <button
               type="button"
-              class="btn-close"
+              class="close"
               data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
+            >X</button>
           </div>
           <div class="modal-body">
-            <img className="img-fluid" src={monirul?.photo} alt="" />
+
+            <img style={{ transform: `scale(${zoom})` }} className="img-fluid" src={monirul?.photo} alt="" />
+          </div>
+          <div style={{ border: "none" }} class="modal-footer text-center">
+            <button onClick={handleZoomIn}>Zoom In</button>
+            <button onClick={handleZoomOut}>Zoom Out</button>
           </div>
         </div>
       </div>
